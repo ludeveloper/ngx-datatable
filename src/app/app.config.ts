1e18a6615e25ba659223cad414f01fae68c32338
Lutfullah Ugurer <ludevelopgmail.com@Lutfullahs-MacAir.local>
2024-01-28 08:23:30 +0300

    added actions column and buttons, implemented mat-icon. fixed data loss

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,5 +6,6 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()]
+  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()
+  ]
 };
--- src/app/templates/data-grid/data-grid.component.ts
+++ src/app/templates/data-grid/data-grid.component.ts
@@ -3,6 +3,9 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { ColumnMode } from '../../enums/column-mode.enum';
@@ -10,6 +13,7 @@ import { NgIf } from '@angular/common';
 import { ICities } from '../../interfaces/cities.interface';
 import { LocationService } from '../../services/location.service';
 import { ITableData } from '../../interfaces/table-data.interface';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-data-grid',
@@ -21,6 +25,9 @@ import { ITableData } from '../../interfaces/table-data.interface';
     FormsModule,
     MatInputModule,
     MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
   ],
   templateUrl: './data-grid.component.html',
   styleUrl: './data-grid.component.css',
@@ -54,22 +61,34 @@ export class DataGridComponent {
     localStorage.setItem('tableData', JSON.stringify(updatedData));
     this.locationService.tableDataSubject.next(updatedData);
   }
+  onChangeRow(event: any) {
+    console.log(event)
+  }
 
-  onChange(event: KeyboardEvent | any, rowIndex: any, col: string) {
+  onChange(event: any, rowIndex: any, col: string | object) {
 
     if (event.key == 'Enter') {
       this.editing[rowIndex] = false;
+    } else if (event.type == 'click') {
+      this.editing[rowIndex] = false
     }
 
     this.updateCell(event, rowIndex, col)
   }
 
-  updateCell(event: KeyboardEvent | any, rowIndex: any, col: string) {
-    this.rows[rowIndex][col] = event.target?.value || event.value || '';
-    this.rows = [...this.rows];
+  updateCell(event: any, rowIndex: any, value: string | object) {
+    let col;
+    if (typeof (value) == 'string') {
+      col = value;
+      this.rows[rowIndex][col] = event.target?.value || event.value || event || '';
+    } else {
+      this.rows[rowIndex] = value
+    }
 
+    this.rows = [...this.rows];
     const index: number = this.changedRows?.findIndex((row: any) => row.index === rowIndex)
     // eğer ilgili satırda ilk defa değişiklik yapıldıysa if, else
+
     if (index < 0) {
       this.changedRows.push({
         index: rowIndex,
@@ -77,7 +96,12 @@ export class DataGridComponent {
       })
       this.changedRowsEmitter.emit(this.changedRows);
     } else {
-      this.changedRows[index][col] = this.rows[rowIndex][col];
+      col ?
+        this.changedRows[index][col] = this.rows[rowIndex][col] :
+        this.changedRows[index] = {
+          index: rowIndex,
+          ...this.rows[rowIndex]
+        }
       this.changedRowsEmitter.emit(this.changedRows);
     }
 
